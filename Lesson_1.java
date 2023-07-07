c11bb0893727ae49b0d7459b8dc13e7354116f88
Alex <alex.ignatov@mail.ru>
2023-07-07 14:39:07 +0300

    added the third and fourth solution

--- Lesson_1.java
+++ Lesson_1.java
@@ -9,7 +9,9 @@ public class Lesson_1 {
 
 public static void main(String[] args) {
     //Triangle_number();
-    Prime_numbers();
+    //Prime_numbers();
+    //Calc();
+    Equation();
 }
     
 // 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n) 
@@ -42,9 +44,77 @@ public static void main(String[] args) {
         }
     }
 // 3) Реализовать простой калькулятор
+    static void Calc() {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Input number a: ");
+        double a = keyboard.nextDouble();
+    
+        System.out.print("Enter any operator(+ , - , * , /) ");
+        char operator = keyboard.next().charAt(0);
+        
+        System.out.print("Input number b: ");
+        double b = keyboard.nextDouble();
 
+        keyboard.close();
+
+        switch (operator) {
+    case '+':
+        System.out.println(a + " + " + b + " = " + (a + b));
+        break;
+    case '-':
+        System.out.println(a + " - " + b + " = " + (a - b));
+        break;
+    case '*':
+        System.out.println(a + " x " + b + " = " + (a * b));
+        break;
+    case '/':
+        System.out.println(a + " / " + b + " = " + (a / b));
+        break;
+    default:
+        System.err.println("Invalid Operator Specified.");
+        break;
+}
+    }
 // 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. 
 // Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. 
 // Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
-
+    static void Equation() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Input equation: ");
+        String str = keyboard.nextLine();
+        
+        int i,j;
+        int x1=0;
+        int y1=0;
+        int count=0;
+        String[] words = str.split("\\s");
+        char[] a = words[0].toCharArray();
+        char[] b = words[2].toCharArray();
+               
+        int sum = Integer.parseInt(words[4]);
+          
+        for (i=1; i < 10; i++) {
+            if (a[0] == '?') {
+                x1 = i*10 + Integer.parseInt(String.valueOf(a[1]));
+            } else if (a[1] == '?') {
+                x1 = Integer.parseInt(String.valueOf(a[0]))*10 + i;
+            }
+            for (j=1; j < 10; j++) {
+                if (b[0] == '?') {
+                    y1 = j*10 + Integer.parseInt(String.valueOf(b[1]));
+                } else if (b[1] == '?') {
+                    y1 = Integer.parseInt(String.valueOf(b[0]))*10 + j;
+                }
+                if (x1 + y1 == sum) {
+                    System.out.printf("%d + %d = %d",x1,y1,sum);
+                } else {
+                    count++;
+                }
+            }
+        }
+        if (count != 0) {
+           System.out.print("There is no solution!"); 
+        }
+    }        
 }
